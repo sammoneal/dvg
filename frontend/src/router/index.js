960e950028eeb2903d61a3fbe1feea154c27d43a
Sam O'Neal <sam.oneal@gmail.com>
2023-04-12 15:26:34 -0700

    Working Vue components and routing.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,25 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+
+import Post from "../components/Post";
+import Author from "../components/Author";
+import PostsByTag from "../components/PostsByTag";
+import AllPosts from "../components/AllPosts";
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+  { path: "/author/:username", component: Author },
+  { path: "/post/:slug", component: Post },
+  { path: "/tag/:tag", component: PostsByTag },
+  { path: "/", component: AllPosts },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
