ddff6429af655463924cfebf87cf43b1f4ceb830
Sam O'Neal <sam.oneal@gmail.com>
2023-04-12 20:27:48 -0700

    Replaced vue apollo cli install with manual install.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,28 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: "http://localhost:3020/graphql",
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+const myApp = createApp(App);
+myApp.use(router);
+myApp.use(apolloClient);
+myApp.mount("#app");
