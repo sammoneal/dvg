5ec1c3612f78dda7c22785d75b33b8afca6bc463
Sam O'Neal <sam.oneal@gmail.com>
2023-04-14 00:03:38 -0700

    Fixed http connection.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,7 +11,7 @@ import router from "./router";
 // HTTP connection to the API
 const httpLink = createHttpLink({
   // You should use an absolute URL here
-  uri: "http://localhost:3020/graphql",
+  uri: "http://localhost:8000/graphql",
 });
 
 // Cache implementation
